a4d27bf95caadf1a3789498b616a9ce5dbd34af5
Guilherme Braga <gui-gbo@hotmail.com>
2017-11-14 21:06:21 -0200

    integracao GLPK e adicao de telas

--- trabalho_pratico/src/main/java/br/com/pucminas/otimizacao/trabalho_pratico/TrabalhoPraticoApplication.java
+++ trabalho_pratico/src/main/java/br/com/pucminas/otimizacao/trabalho_pratico/TrabalhoPraticoApplication.java
@@ -2,11 +2,92 @@ package br.com.pucminas.otimizacao.trabalho_pratico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class TrabalhoPraticoApplication {
+public class TrabalhoPraticoApplication extends SpringBootServletInitializer {
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(TrabalhoPraticoApplication.class);
+	    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrabalhoPraticoApplication.class, args);
-	}
+	    public static void main(String[] args) throws Exception {
+	        SpringApplication.run(TrabalhoPraticoApplication.class, args);
+	    }
+//		 glp_prob lp;
+//	        glp_smcp parm;
+//	        SWIGTYPE_p_int ind;
+//	        SWIGTYPE_p_double val;
+//	        int ret;
+//	        try {
+//	            // Create problem
+//	            lp = GLPK.glp_create_prob();
+//	            System.out.println("Problem created");
+//	            GLPK.glp_set_prob_name(lp, "myProblem");
+//	            // Define columns
+//	            GLPK.glp_add_cols(lp, 2);
+//	            GLPK.glp_set_col_name(lp, 1, "x1");
+//	            GLPK.glp_set_col_kind(lp, 1, GLPKConstants.GLP_CV);
+//	            GLPK.glp_set_col_bnds(lp, 1, GLPKConstants.GLP_DB, 0, .5);
+//	            GLPK.glp_set_col_name(lp, 2, "x2");
+//	            GLPK.glp_set_col_kind(lp, 2, GLPKConstants.GLP_CV);
+//	            GLPK.glp_set_col_bnds(lp, 2, GLPKConstants.GLP_DB, 0, .5);
+//	            // Create constraints
+//	            GLPK.glp_add_rows(lp, 1);
+//	            GLPK.glp_set_row_name(lp, 1, "c1");
+//	            GLPK.glp_set_row_bnds(lp, 1, GLPKConstants.GLP_DB, 0, 0.2);
+//	            ind = GLPK.new_intArray(3);
+//	            GLPK.intArray_setitem(ind, 1, 1);
+//	            GLPK.intArray_setitem(ind, 2, 2);
+//	            val = GLPK.new_doubleArray(3);
+//	            GLPK.doubleArray_setitem(val, 1, 1.);
+//	            GLPK.doubleArray_setitem(val, 2, -1.);
+//	            GLPK.glp_set_mat_row(lp, 1, 2, ind, val);
+//	            // Define objective
+//	            GLPK.glp_set_obj_name(lp, "z");
+//	            GLPK.glp_set_obj_dir(lp, GLPKConstants.GLP_MIN);
+//	            GLPK.glp_set_obj_coef(lp, 0, 1.);
+//	            GLPK.glp_set_obj_coef(lp, 1, -.5);
+//	            GLPK.glp_set_obj_coef(lp, 2, .5);
+//	            // Solve model
+//	            parm = new glp_smcp();
+//	            GLPK.glp_init_smcp(parm);
+//	            ret = GLPK.glp_simplex(lp, parm);
+//	            // Retrieve solution
+//	            if (ret == 0) {
+//	                write_lp_solution(lp);
+//	            } else {
+//	                System.out.println("The problem could not be solved");
+//	            }
+//	            // Free memory
+//	            GLPK.glp_delete_prob(lp);
+//	        } catch (GlpkException ex) {
+//	            ex.printStackTrace();
+//	        }
+//	    }
+//	    /**
+//	     * write simplex solution
+//	     * @param lp problem
+//	     */
+//	    static void write_lp_solution(glp_prob lp) {
+//	        int i;
+//	        int n;
+//	        String name;
+//	        double val;
+//	        name = GLPK.glp_get_obj_name(lp);
+//	        val = GLPK.glp_get_obj_val(lp);
+//	        System.out.print(name);
+//	        System.out.print(" = ");
+//	        System.out.println(val);
+//	        n = GLPK.glp_get_num_cols(lp);
+//	        for (i = 1; i <= n; i++) {
+//	            name = GLPK.glp_get_col_name(lp, i);
+//	            val = GLPK.glp_get_col_prim(lp, i);
+//	            System.out.print(name);
+//	            System.out.print(" = ");
+//	            System.out.println(val);
+//	        }
+//	}
 }
---
+++ trabalho_pratico/src/main/java/br/com/pucminas/otimizacao/trabalho_pratico/controller/MainController.java
@@ -0,0 +1,17 @@
+package br.com.pucminas.otimizacao.trabalho_pratico.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+	
+}
